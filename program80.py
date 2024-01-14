20fe45f5a7a2106aedf1fa8e8540c8d30429eb75
Jonnalagadda Surya Kiran <kiransurya93@kluniversity.in>
2024-01-14 15:14:04 +0530

    Add files via upload

---
+++ program80.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def display():
+    return "Hello World"
+
+# program80 is a custom module, in which we can access add(),sub(),mul(),display() functions
